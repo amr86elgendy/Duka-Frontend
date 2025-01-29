b3c37f3d731a8ff5796ea1f871d18bf2a4e7bec4
Amr Elgendy <67813463+amr86elgendy@users.noreply.github.com>
2025-01-29 21:38:55 +0200

    Update NewArrival.tsx

--- src/components/home/NewArrival.tsx
+++ src/components/home/NewArrival.tsx
@@ -1,16 +1,7 @@
 import ProductCard from './productCard';
-import { useGetProducts } from '@/apis/shopping';
 import Skeleton from '@/components/home/productCard/Skeleton';
 
 export default function NewArrival() {
-  const queries = {
-    sort: '-createdAt',
-    limit: 4,
-  };
-  const productsQuery = useGetProducts({ queries });
-  const products =
-    productsQuery.data?.pages.flatMap((page) => page.products) || [];
-
   return (
     <div className="mb-12 grid grid-cols-[25%,1fr] overflow-hidden rounded-md border-4 border-red-500 bg-red-500 text-white">
       <div className="p-10 ">
@@ -25,11 +16,6 @@ export default function NewArrival() {
         >
           See More
         </button>
-      </div>
-      <div className="grid grid-cols-4 overflow-hidden rounded-lg bg-gray-200">
-        {productsQuery.isLoading
-          ? [...Array(4).keys()].map((el) => <Skeleton key={el} />)
-          : products.map((p) => <ProductCard {...p} key={p._id} />)}
       </div>
     </div>
   );
